377a08a914f3a2c4212045dd5fd90ed0be057e28
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-07-24 17:29:55 +0200

    milestone 1 terminata

---
+++ laravel/resources/views/guest/comics/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title','comics list')
+
+@section('main-content')
+<h1>
+    homePage
+</h1>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController as GuestPageController;
+use App\Http\Controllers\Guest\ComicController as GuestComicController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/',[GuestPageController::class,'home'])->name('guest.home');
+Route::get('/comics',[GuestComicController::class,'index'])->name('guest.comics.index');
